8d1a4303ebb506d96064ffd95ef0ca13be34009e
elikfir@gmail.com <elikfir@gmail.com@e8dd7c8f-6a4b-77aa-af92-0c4c70cc00f7>
2011-04-13 21:37:57 +0000

    first heuristics

--- trunk/ai1/src/shplechtz.py
+++ trunk/ai1/src/shplechtz.py
@@ -1,15 +1,20 @@
 from problem_agent import ProblemAgent
-from search.best_first import BestFirstGraphSearch
+from search.beam_search import BeamSearch
 from heuristics import *
 from boards import *
 import time
+from search.beam_search import BeamSearch
 
 class RobotsAgent(ProblemAgent):
     def solve(self, problem_state, time_limit):
-        return BestFirstGraphSearch().find(problem_state, CleanHeuristic())
+        #return BeamSearch().find(problem_state, CleanHeuristic())
+        #return BeamSearch().find(problem_state, BdioHeuristic())
+        return BeamSearch().find(problem_state, BlaHeuristic())
 
 if __name__ == '__main__': 
-    problem = generate_medium_board(10, 10, 2)
+    random.seed(time.clock())
+    #problem = generate_medium_board(20, 20, 1)
+    problem = generate_debug_board()
     print problem
     
     agent = RobotsAgent()
