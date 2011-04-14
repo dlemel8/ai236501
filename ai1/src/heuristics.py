d1aa55442438e7f3b8d9fad4bca7bc54b1fd726b
dlemel8 <dlemel8@e8dd7c8f-6a4b-77aa-af92-0c4c70cc00f7>
2011-04-14 08:28:44 +0000

    now it's look like python

--- ai1/src/heuristics.py
+++ ai1/src/heuristics.py
@@ -4,38 +4,36 @@ from search.utils import infinity
 from copy import deepcopy
 import sys
 
+def die(str):
+    print '[KOL HABASA] ' + str
+    sys.exit(0)
+
 class CleanHeuristic(Heuristic):
     def evaluate(self, state):
         return len(state.dirt_locations)
     
-class BdioHeuristic(Heuristic):
+class ShortestPathHeuristic(Heuristic):
+    def find_solution(self, state, dirt):
+        state.dirt_locations = frozenset([dirt])
+        solution = BreadthFirstGraphSearch().find(state, 5)
+        if not solution:
+            die("BreadthFirstGraphSearch didn't found solution")
+        return solution
+    
     def evaluate(self, state):
         realDirts = deepcopy(state.dirt_locations)
-        minVal = state.width * state.height
-        for dirt in realDirts:
-            state.dirt_locations = frozenset([dirt])
-            solution = BreadthFirstGraphSearch().find(state, 5)
-            if not solution:
-                print "KOL HABASA"
-                sys.exit(0)
-            minVal = min(len(solution), minVal)
-        
-        state.dirt_locations = realDirts 
+        sols = [len(self.find_solution(state, dirt)) for dirt in realDirts]
+        minVal = reduce(min, sols, state.width * state.height)
+        state.dirt_locations = realDirts
         return minVal + state.width * state.height * len(state.dirt_locations)
 
-class BlaHeuristic(Heuristic):
+class IgnoreObstaclesHeuristic(Heuristic):
     def dist(self, p1, p2):
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
     
     def evaluate(self, state):
-        
         totalMinVal = state.width * state.height * len(state.dirt_locations) * len(state.robots)
-        
         for robot in state.robots:
-            minVal = state.width * state.height
-            for dirt in state.dirt_locations:
-                minVal = min(minVal, self.dist(dirt, robot))
-            
-            totalMinVal += minVal
-            
+            dists = [self.dist(dirt, robot) for dirt in state.dirt_locations]
+            totalMinVal += reduce(min, dists, state.width * state.height)
         return totalMinVal
