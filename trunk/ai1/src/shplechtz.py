24ab2bb6b96e34d214b8883246369f3b9c975b0a
elikfir@gmail.com <elikfir@gmail.com@e8dd7c8f-6a4b-77aa-af92-0c4c70cc00f7>
2011-04-21 18:56:16 +0000

    added new heuristics - OneDirtPerRobotHeuristic and OneDirtPerRobotShortestHeuristic

--- trunk/ai1/src/shplechtz.py
+++ trunk/ai1/src/shplechtz.py
@@ -11,14 +11,16 @@ class RobotsAgent(ProblemAgent):
     def solve(self, problem_state, time_limit):
         #return BeamSearch().find(problem_state, CleanHeuristic())
         #return BeamSearchAnyTime(time_limit).find(problem_state, ShortestPathHeuristic())
-        return BeamSearchAnyTime(time_limit).find(problem_state, IgnoreObstaclesHeuristic())
+        #return BeamSearchAnyTime(time_limit).find(problem_state, IgnoreObstaclesHeuristic())
+        #return BeamSearchAnyTime(time_limit).find(problem_state, OneDirtPerRobotHeuristic())
+        return BeamSearchAnyTime(time_limit).find(problem_state, OneDirtPerRobotShortestHeuristic())
         #return BeamSearchAnyTime(time_limit).find(problem_state, AllmostShortestPathHeuristic())
         #return AStarAnyTime(time_limit).find(problem_state, IgnoreObstaclesHeuristic())
         #return AStarAnyTime(time_limit).find(problem_state, ShortestPathHeuristic())
 
 if __name__ == '__main__': 
     random.seed(time.clock())
-    problem = generate_medium_board(10, 10, 4)
+    problem = generate_hard_board(10, 10, 3)
     #problem = generate_debug_board()
     print problem
     
