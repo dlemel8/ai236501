d4759417445c376cbcbdfcd666b93cef76457694
elikfir@gmail.com <elikfir@gmail.com@e8dd7c8f-6a4b-77aa-af92-0c4c70cc00f7>
2011-04-21 18:56:16 +0000

    added new heuristics - OneDirtPerRobotHeuristic and OneDirtPerRobotShortestHeuristic

--- ai1/src/heuristics.py
+++ ai1/src/heuristics.py
@@ -19,6 +19,7 @@ class ShortestPathHeuristic(Heuristic):
         state.dirt_locations = frozenset([dirt])
         solution = BreadthFirstGraphSearch().find(state, 5)
         if not solution:
+            # fix it !!!!
             die("BreadthFirstGraphSearch didn't found solution")
         return solution
     
@@ -43,6 +44,7 @@ class AllmostShortestPathHeuristic(Heuristic):
         state.dirt_locations = realDirts
         state.robots = realRobots
         if not solution:
+            # fix it !!!!
             die("BreadthFirstGraphSearch didn't found solution")
         return solution
     
@@ -69,4 +71,60 @@ class IgnoreObstaclesHeuristic(Heuristic):
         for robot in state.robots:
             dists = [self.dist(dirt, robot) for dirt in state.dirt_locations]
             totalMinVal += reduce(min, dists, state.width * state.height)
+        return totalMinVal
+    
+class OneDirtPerRobotHeuristic(Heuristic):
+    def dist(self, p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+    
+    def evaluate(self, state):
+        totalMinVal = state.width * state.height * len(state.dirt_locations) * len(state.robots)
+        robots = list(deepcopy(state.robots))
+        dirts = list(deepcopy(state.dirt_locations))
+        while len(dirts) and len(robots):
+            firstMinVal = infinity
+            for (dirt, robot) in itertools.product(dirts, robots):
+                tmpVal = self.dist(dirt, robot)
+                if (tmpVal < firstMinVal):
+                    firstMinVal = tmpVal
+                    minDirt = dirt
+                    minRobot = robot
+            robots.remove(minRobot)
+            dirts.remove(minDirt)
+            totalMinVal += self.dist(minDirt, minRobot)
+        return totalMinVal
+    
+class OneDirtPerRobotShortestHeuristic(Heuristic):
+    def dist(self, p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+    
+    def find_solution(self, state, dirt, robot):
+        realDirts = deepcopy(state.dirt_locations)
+        realRobots = deepcopy(state.robots)
+        state.dirt_locations = frozenset([dirt])
+        state.robots = tuple([robot])
+        #solution = BreadthFirstGraphSearch().find(state, 5)
+        solution = BeamSearch().find(state, IgnoreObstaclesHeuristic())
+        state.dirt_locations = realDirts
+        state.robots = realRobots
+        if not solution:
+            # fix it !!!!
+            die("BreadthFirstGraphSearch didn't found solution")
+        return solution
+    
+    def evaluate(self, state):
+        totalMinVal = state.width * state.height * len(state.dirt_locations) * len(state.robots)
+        robots = list(deepcopy(state.robots))
+        dirts = list(deepcopy(state.dirt_locations))
+        while len(dirts) and len(robots):
+            firstMinVal = infinity
+            for (dirt, robot) in itertools.product(dirts, robots):
+                tmpVal = self.dist(dirt, robot)
+                if (tmpVal < firstMinVal):
+                    firstMinVal = tmpVal
+                    minDirt = dirt
+                    minRobot = robot
+            robots.remove(minRobot)
+            dirts.remove(minDirt)
+            totalMinVal += len(self.find_solution(state, minDirt, minRobot))
         return totalMinVal
